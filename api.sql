7d39459ee20a591af50c88866ce2e7309f391cb5
osmcgrgenc <ocgenc@gmail.com>
2021-01-29 09:59:17 +0300

    SQL changed

--- api.sql
+++ api.sql
@@ -1,60 +1,35 @@
 CREATE DATABASE IF NOT EXISTS `codeigniter3_api` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `codeigniter3_api`;
 
-CREATE TABLE IF NOT EXISTS `product` (
+CREATE TABLE IF NOT EXISTS `berbers` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `title` text COLLATE utf8_unicode_ci,
+  `website` text COLLATE utf8_unicode_ci,
+  `address` text COLLATE utf8_unicode_ci,
+  `phone` text COLLATE utf8_unicode_ci,
+  `logo` text COLLATE utf8_unicode_ci,
+  `isDeleted` text COLLATE utf8_unicode_ci,
+  `created_at` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=51 ;
 
-INSERT INTO `product` (`id`, `title`) VALUES
-(1, 'ipsum'),
-(2, 'egestas.'),
-(3, 'a'),
-(4, 'erat'),
-(5, 'augue.'),
-(6, 'quis'),
-(7, 'Cras'),
-(8, 'aliquet'),
-(9, 'Phasellus'),
-(10, 'varius'),
-(11, 'In'),
-(12, 'in'),
-(13, 'cubilia'),
-(14, 'eget,'),
-(15, 'Sed'),
-(16, 'ornare.'),
-(17, 'euismod'),
-(18, 'eget'),
-(19, 'imperdiet'),
-(20, 'eu'),
-(21, 'eget'),
-(22, 'a'),
-(23, 'sodales'),
-(24, 'Phasellus'),
-(25, 'Curabitur'),
-(26, 'neque'),
-(27, 'in'),
-(28, 'nascetur'),
-(29, 'pharetra'),
-(30, 'euismod'),
-(31, 'amet'),
-(32, 'egestas'),
-(33, 'arcu.'),
-(34, 'metus'),
-(35, 'egestas,'),
-(36, 'est.'),
-(37, 'amet'),
-(38, 'amet'),
-(39, 'luctus'),
-(40, 'Proin'),
-(41, 'volutpat'),
-(42, 'blandit'),
-(43, 'Aenean'),
-(44, 'vulputate,'),
-(45, 'interdum'),
-(46, 'parturient'),
-(47, 'lobortis,'),
-(48, 'Cras'),
-(49, 'dictum'),
-(50, 'Sed');
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `username` text COLLATE utf8_unicode_ci,
+  `password` text COLLATE utf8_unicode_ci,
+  `email` text COLLATE utf8_unicode_ci,
+  `phone` text COLLATE utf8_unicode_ci,
+  `avatar` text COLLATE utf8_unicode_ci,
+  `fullName` text COLLATE utf8_unicode_ci,
+  `role` text COLLATE utf8_unicode_ci,
+  `isDeleted` text COLLATE utf8_unicode_ci,
+  `created_at` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=51 ;
+
+INSERT INTO `berbers` (`id`, `title`,`website`,`address`,`phone`,`logo`,`isDeleted`,`created_at`,) VALUES
+(1, 'Berber 1', 'https://osmancagrigenc.com', 'Konya', '5459077765', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1'),
+(2, 'Berber 1', 'https://osmancagrigenc.com', 'Konya', '5459077765', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1'),
+(3, 'Berber 1', 'https://osmancagrigenc.com', 'Konya', '5459077765', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1'),
+(4, 'Berber 1', 'https://osmancagrigenc.com', 'Konya', '5459077765', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1'),
+(5, 'Berber 1', 'https://osmancagrigenc.com', 'Konya', '5459077765', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnetflixroulette.files.wordpress.com%2F2013%2F01%2Fimage-not-found.gif&f=1&nofb=1');
